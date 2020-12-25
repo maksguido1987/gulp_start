44f9b2adc0346dcf0a6c27d1eb87806360e5a0c3
Maksim <maksguido@gmail.com>
2020-12-25 18:49:35 +0300

    correct webp format

--- gulpfile.js
+++ gulpfile.js
@@ -153,22 +153,22 @@ gulp.task('svgsprite', function () {
 function fontsStyle(params) {
 
    let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
-   if (file_content == '') {
-   fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
-   return fs.readdir(path.build.fonts, function (err, items) {
-   if (items) {
-   let c_fontname;
-   for (var i = 0; i < items.length; i++) {
-   let fontname = items[i].split('.');
-   fontname = fontname[0];
-   if (c_fontname != fontname) {
-   fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
-   }
-   c_fontname = fontname;
-   }
-   }
-   })
-   }
+      if (file_content == '') {
+         fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+            return fs.readdir(path.build.fonts, function (err, items) {
+               if (items) {
+               let c_fontname;
+                  for (var i = 0; i < items.length; i++) {
+                  let fontname = items[i].split('.');
+                  fontname = fontname[0];
+                  if (c_fontname != fontname) {
+                  fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+                  }
+                  c_fontname = fontname;
+                  }
+               }
+         })
+      }
    }
 function cb() {
 
